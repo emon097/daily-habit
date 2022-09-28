ae994e66888fa36f1128225ea68bde330981e754
emon097 <mdemonhossain9527@gmail.com>
2022-09-28 22:37:20 +0600

    add-single-cart

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "../Nav/Nav";
 import Cart from "../Single/Cart/Cart";
 import Single from "../Single/Single";
 import "./Home.css";
 const Home = () => {
+  const [single, setSingle] = useState([]);
+  useEffect(() => {
+    fetch("time.json")
+      .then((res) => res.json())
+      .then((data) => setSingle(data));
+  }, []);
   return (
     <div className="home-js">
       <Nav></Nav>
       <h2> Select today’s Rutin </h2>
       <div className="header">
-        <Single></Single>
-        <Cart></Cart>
+        <div className="single-cart1">
+          {single.map((singles) => (
+            <Single single={singles} key={singles.id}></Single>
+          ))}
+        </div>
+        <div>
+          <Cart></Cart>
+        </div>
       </div>
     </div>
   );
--- src/components/Single/Single.js
+++ src/components/Single/Single.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./Single.css";
-const Single = () => {
+const Single = (single) => {
+  const { Age, description, img, name, time } = single.single;
   return (
-    <div>
-      <h1>single cart</h1>
+    <div className="single-cart">
+      <img className="cart-img" src={img} alt="" />
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>For age: {Age}</p>
+      <p>Time required: {time}</p>
+      <button>Add to list</button>
     </div>
   );
 };
