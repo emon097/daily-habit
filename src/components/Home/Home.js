361c65ba320d52e4ddb9a9dc48f392640a5619ba
emon097 <mdemonhossain9527@gmail.com>
2022-09-28 21:39:04 +0600

    add logo

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../Nav/Nav";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div>
+      <Nav></Nav>
+      <h1>hi im from home</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Nav.css";
+const Nav = () => {
+  return (
+    <div className="logo">
+      <img
+        className="image-name"
+        src="https://t3.ftcdn.net/jpg/02/96/27/28/240_F_296272864_7XXe0wavzRXA9zKSkzXxhmZhQbyY8b26.jpg"
+        alt=""
+      />
+      <h1> DAILY-HABIT.COM</h1>
+    </div>
+  );
+};
+
+export default Nav;
